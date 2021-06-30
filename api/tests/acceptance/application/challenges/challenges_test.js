bd0109fe213318ef429464547a04bf5bb8931fc5
jeremie <jeremie.jade@pix.fr>
2021-06-30 15:47:38 +0200

    Add post challenge
    Co-authored-by: Guillaume Lagorce <guillaume.lagorce@pix.fr>
    Co-authored-by: Thomas Evano <thomas.evano@pix.fr>
    Co-authored-by: François de Metz <francois.de-metz@pix.fr>

--- api/tests/acceptance/application/challenges/challenges_test.js
+++ api/tests/acceptance/application/challenges/challenges_test.js
@@ -141,7 +141,8 @@ describe('Acceptance | Controller | challenges-controller', () => {
       });
 
       // Then
-      airtableCall.isDone();
+      expect(airtableCall.isDone()).to.be.true;
+
       expect(response.statusCode).to.equal(200);
       expect(response.result).to.deep.equal({
         data: [
@@ -406,7 +407,91 @@ describe('Acceptance | Controller | challenges-controller', () => {
       expect(response.statusCode).to.equal(404);
       expect(airtableCall.isDone()).to.be.true;
     });
+  });
 
+  describe('POST /challenges', () => {
+    let user;
+    beforeEach(async function() {
+      user = databaseBuilder.factory.buildAdminUser();
+      await databaseBuilder.commit();
+    });
+
+    function _removeReadonlyFields(airtableChallengeBody) {
+      delete airtableChallengeBody.fields.Preview;
+      delete airtableChallengeBody.fields['Record ID'];
+      delete airtableChallengeBody.fields['Compétences (via tube) (id persistant)'];
+      delete airtableChallengeBody.fields['Acquix (id persistant)'];
+      delete airtableChallengeBody.fields['Scoring'];
+      return airtableChallengeBody;
+    }
+
+    it('should create a challenge', async () => {
+      // Given
+      const challenge = domainBuilder.buildChallenge({ id: 'challengeId' });
+      const expectedBodyChallenge = _removeReadonlyFields(airtableBuilder.factory.buildChallenge(challenge));
+      const expectedBody = { records: [expectedBodyChallenge] };
+
+      const airtableCall = nock('https://api.airtable.com')
+        .post('/v0/airtableBaseValue/Epreuves/?', expectedBody)
+        .reply(
+          200,
+          { records: [airtableBuilder.factory.buildChallenge(challenge)] }
+        );
+      const server = await createServer();
+
+      // When
+      const response = await server.inject({
+        method: 'POST',
+        url: '/api/challenges',
+        headers: generateAuthorizationHeader(user),
+        // TODO: send a challenge serialized to JSONAPI
+        payload: challenge,
+      });
+
+      // Then
+      expect(airtableCall.isDone()).to.be.true;
+      expect(response.statusCode).to.equal(201);
+      expect(response.result).to.deep.equal({
+        data: {
+          type: 'challenges',
+          id: 'challengeId',
+          attributes: {
+            'airtable-id': challenge.airtableId,
+            instruction: 'Les moteurs de recherche affichent certains liens en raison d\'un accord commercial.\n\nDans quels encadrés se trouvent ces liens ?',
+            'alternative-instruction': '',
+            type: 'QCM',
+            format: 'mots',
+            proposals: '- 1\n- 2\n- 3\n- 4\n- 5',
+            solution: '1, 5',
+            'solution-to-display': '1',
+            't1-status': 'Activé',
+            't2-status': 'Désactivé',
+            't3-status': 'Activé',
+            pedagogy: 'q-situation',
+            author: ['SPS'],
+            declinable: 'facilement',
+            version: 1,
+            genealogy: 'Prototype 1',
+            status: 'validé',
+            preview: 'http://staging.pix.fr/challenges/recwWzTquPlvIl4So/preview',
+            scoring: '1: @outilsTexte2\n2: @outilsTexte4',
+            timer: 1234,
+            'embed-url': 'https://github.io/page/epreuve.html',
+            'embed-title': 'Epreuve de selection de dossier',
+            'embed-height': 500,
+            'alternative-version': 2,
+            accessibility1: 'OK',
+            accessibility2: 'RAS',
+            spoil: 'Non Sp',
+            responsive:  'non',
+            locales: [],
+            area: 'France',
+            'auto-reply': false,
+            focusable: false,
+          }
+        },
+      });
+    });
   });
 
 });
